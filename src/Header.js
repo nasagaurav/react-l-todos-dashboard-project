dd50309e11792390611851dc8e98e5527f955484
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-11 07:51:13 +0530

    header

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDisptch } from 'react-redux';
+function Header() {
+  const state = useSelector((s) => s);
+  const dispatch = useDisptch();
+  const { loggedin, token, user } = state;
+
+  const logout = () => {
+    dispatch({ type: 'logout' });
+  };
+
+  return (
+    <div>
+      <div>Logoname</div>
+      <div>
+        <Link to="/">Home </Link>
+        {!loggedin && <Link to="/Login">Login </Link>}
+        {!loggedin && <Link to="/Signup">Signup </Link>}
+        {loggedin && <Link to="/Dashboard">Dashboard</Link>}
+        {loggedin && <a onClick={logout}>Logout ({user})</a>}
+      </div>
+    </div>
+  );
+}
+export default Header;
