0aac0b7de2dd336b7a842f6a309b0b3e70977be3
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-11 08:22:24 +0530

    services added create,update,del

--- src/services.js
+++ src/services.js
@@ -27,3 +27,24 @@ export const signupService = async (payload) => {
   const result = await axios.post(url, payload).then((res) => res.data);
   return result;
 };
+export const create = async (payload, token) => {
+  const url = 'https://vast-tor-96424.herokuapp.com/mynotes';
+  const result = await axios
+    .post(url, getToken(token), payload)
+    .then((res) => res.data);
+  return result;
+};
+export const update = async (id, status, token) => {
+  const url = `https://vast-tor-96424.herokuapp.com/mynotes/${id}/${status}`;
+  const result = await axios
+    .patch(url, getToken(token))
+    .then((res) => res.data);
+  return result;
+};
+export const del = async (id, token) => {
+  const url = `https://vast-tor-96424.herokuapp.com/mynotes/${id}`;
+  const result = await axios
+    .delete(url, getToken(token))
+    .then((res) => res.data);
+  return result;
+};
