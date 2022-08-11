ac6fe7197e24d09fa3ba00001ea7e747ebf1853e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-11 07:49:33 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Header from './Header';
+import Footer from './Footer';
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Signup" element={<Signup />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
+export default App;
