503b8ee355dd1228980c44de8ef4268e9dfab8ae
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-11 09:07:01 +0530

    added table from mu in dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,14 @@
 import { toast } from 'react-toastify';
-
+import B from './b';
+import Container from '@mui/material/Container';
+import Alert from '@mui/material/Alert';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import React, { useState, useEffect, useRef } from 'react';
 import { create, update, del, view } from './services';
 import { useSelector } from 'react-redux';
@@ -60,40 +69,49 @@ function Dashboard() {
   };
   useEffect(loadMyTodos, []);
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <Container>
+      <br />
+      <B />
+      <br />
       <div className="form">
         <h3>create new todo </h3>
         <input placeholder="title" ref={r1} />
         <input placeholder="description" ref={r2} />
         <button onClick={insert}>insert</button>
       </div>
-      <h3>My todos {a.length}</h3>
 
-      <table>
-        <thead>
-          <tr>
-            <th>title</th>
-            <th>description</th>
-            <th>status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {a.map((x) => (
-            <tr>
-              <td>{x.title}</td>
-              <td>{x.description}</td>
-              <td>
-                <button onClick={() => _update(x.id, !x.status)}>
-                  {x.status ? 'completed' : 'pending'}
-                </button>
-                <button onClick={() => _delete(x.id)}>delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <Alert severity="success">My todos {a.length}!</Alert>
+      {/* table starts */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>title</TableCell>
+              <TableCell>description</TableCell>
+              <TableCell>status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {a.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="right">{row.title}</TableCell>
+                <TableCell align="right">{row.description}</TableCell>
+                <TableCell align="right">
+                  <button onClick={() => _update(row.id, !row.status)}>
+                    {row.status ? 'completed' : 'pending'}
+                  </button>
+                  <button onClick={() => _delete(row.id)}>delete</button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* table ends */}
+    </Container>
   );
 }
 export default Dashboard;
